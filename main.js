2fd173fdd5d3ea471dfcaba2995bcec7baeef33b
labstraction <45163263+labstraction@users.noreply.github.com>
2023-06-20 10:39:59 +0200

    intro tipi

---
+++ main.js
@@ -0,0 +1,96 @@
+console.log('hello world!')
+
+// Tipi di base ---------------------------------------
+
+// 'feefevfd' // string
+
+// 34 56.6 -23 // number
+
+// true false // boolean
+
+// Tipi strani:
+
+// NaN Not a Number
+
+// variabili con let----------------------------------- 
+
+// let pippo = 5;
+
+// console.log(typeof pippo);
+
+// pippo = 'viva il caffè!';
+
+// console.log(typeof pippo);
+
+// console.log(pippo)
+
+
+// variabili con const----------------------------------- 
+
+// const pluto = 12;
+
+// pluto = 23;
+
+// console.log(pluto);
+
+
+// variabili con var (guai a chi le usa! vi taglio le mani!!!)---------
+
+var paperino = true;
+
+console.log(paperino);
+
+paperino = -123;
+
+console.log(paperino);
+
+// differenza con let
+
+// topolino = 5;
+
+// let topolino;
+
+// minnie = 5;
+
+// var minnie;
+
+// console.log(minnie);
+
+
+// Number -----------------------------------------------------
+
+let qui = (5 + 9);
+let qua = (5 * 9);
+let quo = (5 / 9);
+
+let paperone = 5 ** 3; // elevamento a potenza
+
+let basettoni = 6 % 5;  // modulo
+
+console.log(qui);
+console.log(qua);
+console.log(quo);
+
+console.log(paperone);
+
+console.log(basettoni);
+
+let gambaDiLegno = 8 % 2;
+
+console.log(gambaDiLegno);
+
+let ilNumeroPiuPiccolo = -Infinity;
+
+let ilNumeroPiuGrande = Infinity;
+
+const paperoga = Math.sqrt(16); // libreria Math per tutte le operazioni non comprese nello standard ES
+
+console.log(paperoga);
+
+const rockerduck = 'pippo' / 3;
+
+console.log(rockerduck);
+
+
+// String ------------------------------------------------------------------------------------
+
